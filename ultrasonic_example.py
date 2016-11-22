e4c1ed96da844aaa38e729db59e1b7d0def9da47
Fletcher3003 <charliehouseago@gmail.com>
2016-11-22 14:50:05 +0000

    Playing with Ultrasound

--- ultrasonic_example.py
+++ ultrasonic_example.py
@@ -4,7 +4,7 @@ import time
 interface=brickpi.Interface()
 interface.initialize()
 
-port = 0 # port which ultrasoic sensor is plugged in to
+port = 1 # port which ultrasonic sensor is plugged in to
 
 interface.sensorEnable(port, brickpi.SensorType.SENSOR_ULTRASONIC);
 
