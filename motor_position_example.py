a46970154f5da7d845828fee2b2649b61f201bc1
Fletcher3003 <ch516@ic.ac.uk>
2016-11-22 12:59:20 +0000

    PID first test

--- motor_position_example.py
+++ motor_position_example.py
@@ -16,9 +16,9 @@ motorParams.feedForwardGain = 255/20.0
 motorParams.minPWM = 18.0
 motorParams.pidParameters.minOutput = -255
 motorParams.pidParameters.maxOutput = 255
-motorParams.pidParameters.k_p = 100.0
-motorParams.pidParameters.k_i = 0.0
-motorParams.pidParameters.k_d = 0.0
+motorParams.pidParameters.k_p = 300
+motorParams.pidParameters.k_i = 600
+motorParams.pidParameters.k_d = 50
 
 interface.setMotorAngleControllerParameters(motors[0],motorParams)
 interface.setMotorAngleControllerParameters(motors[1],motorParams)
