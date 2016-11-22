8d9cbac720a8433b81bdee6a8377e0d05fa0b0cd
Fletcher3003 <charliehouseago@gmail.com>
2016-11-22 14:00:20 +0000

    PID Tuned

--- params.py
+++ params.py
@@ -6,6 +6,6 @@ minPWM = 18.0
 # Pid Params
 minOutput = -255
 maxOutput = 255
-k_p = 400.0
-k_i = 0.0
-k_d = 6.0
+k_p = 480.0
+k_i = 250.0
+k_d = 9.6
--- pid_test.py
+++ pid_test.py
@@ -28,10 +28,10 @@ motorParams.pidParameters.k_d = params.k_d
 interface.setMotorAngleControllerParameters(motors[0],motorParams)
 interface.setMotorAngleControllerParameters(motors[1],motorParams)
 
+interface.startLogging('pidtest.dat')
 while True :
 	angle = float(input("Enter an angle to rotate (in radians): "))
-
-	interface.startLogging('pidtest.dat')	
+	
 	interface.increaseMotorAngleReferences(motors,[angle,angle])
 	
 	while not interface.motorAngleReferencesReached(motors) :
@@ -39,7 +39,8 @@ while True :
 		if motorAngles :
 			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
 		time.sleep(0.1)
-	interface.stopLogging()
+	
 	print "Destination reached!"
 
+interface.stopLogging()
 interface.terminate()
